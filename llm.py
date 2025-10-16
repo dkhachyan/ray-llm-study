ab87da10f568fb05bed8f9f02c78bd4ec0abb6fe
Denis Khachyan <khachyanda@gmail.com>
2025-10-16 10:25:40 +0300

    add sleep mode

--- llm.py
+++ llm.py
@@ -5,11 +5,13 @@ from vllm import LLM, SamplingParams
 @serve.deployment()
 class HelloWorld:
     def __init__(self):
-        self.model = LLM("Qwen/Qwen2.5-0.5B-Instruct")
+        self.model = LLM("Qwen/Qwen2.5-0.5B-Instruct", enable_sleep_mode=True)
         self.sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
         self.promt="privet kto kotik"
     async def __call__(self, request: Request):
+        self.model.wake_up()
         outputs = self.model.generate(self.promt, self.sampling_params)
+        self.model.sleep(level=2)
         return outputs[0].outputs[0].text
 
 app = HelloWorld.bind()
