77dca38a166b86a37d463edd7acb86a667197ff4
Denis Khachyan <khachyanda@gmail.com>
2025-10-17 09:32:30 +0300

    vllm v1

--- llm.py
+++ llm.py
@@ -7,11 +7,14 @@ class HelloWorld:
     def __init__(self):
         self.model = LLM("Qwen/Qwen2.5-0.5B-Instruct", enable_sleep_mode=True)
         self.sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
-        self.promt="privet kto kotik"
+        self.model.reset_prefix_cache()
+        self.model.sleep(level=1)
+        self.promt="Hi. Who are you"
     async def __call__(self, request: Request):
         self.model.wake_up()
         outputs = self.model.generate(self.promt, self.sampling_params)
-        self.model.sleep(level=2)
+        self.model.reset_prefix_cache()
+        self.model.sleep(level=1)
         return outputs[0].outputs[0].text
 
 app = HelloWorld.bind()
